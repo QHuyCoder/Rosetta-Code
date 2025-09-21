fa2d4e119252cc73b986f83a02d858e3f9240e73
Huyvq <139686652+QHuyCoder@users.noreply.github.com>
2025-09-21 22:50:29 +0700

    Update main.js

--- 100_doors/main.js
+++ 100_doors/main.js
@@ -1,1 +1,32 @@
+function getFinalOpenedDoors(numDoors) {
+  let result = [];
+  for (let i = 1; i * i <= numDoors; i++) {
+    result.push(i * i);
+  }
+  return result;
+}
+
+document.getElementById("runBtn").addEventListener("click", () => {
+  const numDoors = 100;
+  const opened = getFinalOpenedDoors(numDoors);
+  const output = document.getElementById("output");
+
+  output.innerHTML = ""; // clear cũ
+
+  
+
+  for (let i = 1; i <= numDoors; i++) {
+    const door = document.createElement("div");
+    door.classList.add("door");
+    door.textContent = i;
+
+    if (opened.includes(i)) {
+      door.classList.add("open");
+    } else {
+      door.classList.add("closed");
+    }
+
+    output.appendChild(door);
+  }
+});
 
